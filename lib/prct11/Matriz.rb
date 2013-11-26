c31a7bbebd0b293a1961c68ff033dc5f46ff51b8
Leandro Ponte <alu0100724165@ull.edu.es>
2013-11-26 22:40:19 +0000

    Se ha sobrecargado el operador + pero falta sobrecargar el operador = y [] para que este correcto

--- lib/prct11/Matriz.rb
+++ lib/prct11/Matriz.rb
@@ -4,8 +4,32 @@ require "prct11/fraccion.rb"
 
 class Matriz
 
-	def initialize
-               
+	attr_reader :filas,:columnas
+
+	def initialize(matriz)
+	
+		@filas = matriz.length #filas
+       	 	@columnas = matriz[0].length #Columnas
+		#Creamos la matriz
+           	@matriz = Array.new(@filas){Array.new(@columnas)}
+           	#Cargamos la matriz
+			for i in 0...@filas
+	                       for j in 0...@columnas
+		                  @matriz[i][j] = matriz[i][j]
+	                       end
+			end
+        end
+
+	
+	def +(m2)
+		result = Array.new(@filas){Array.new(@columnas)}
+		
+		for i in 0...@filas
+		        for j in 0...@columnas
+		                result[i][j] = @matriz[i][j] + m2[i][j]
+		        end
+		end
+		return Matriz.new(result)
 	end
        
 	
--- spec/Matriz_spec.rb
+++ spec/Matriz_spec.rb
@@ -4,6 +4,13 @@ require "prct11/fraccion.rb"
 describe Matriz do
  before :each do
     
-  end 
+	@matriz1 = Matriz.new([[1,2],[3,4]])
+	@matriz2 = Matriz.new([[5, 6],[7,8]])
 
+  end 
+	describe "Operaciones basicas con matrices" do
+    		it "Suma de Matrices" do
+        		(@matriz1 + @matriz2).should == Matriz.new([[6,8],[10,12]])
+    		end
+	end
 end
