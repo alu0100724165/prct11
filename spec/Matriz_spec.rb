b960833de3eedc7dab185ad89b1ce6769aad7b47
Leandro Ponte <alu0100724165@ull.edu.es>
2013-11-26 23:44:12 +0000

    Se ha sobrecargado el operador * y la expectativa se resuelve correctamente. La suma y multiplicación de matrices se hace correctamente

--- spec/Matriz_spec.rb
+++ spec/Matriz_spec.rb
@@ -8,9 +8,12 @@ describe Matriz do
 	@matriz2 = Matriz.new([[5, 6],[7,8]])
 
   end 
-	describe "Operaciones basicas con matrices" do
+	describe "Operaciones basicas" do
     		it "Suma de Matrices" do
         		(@matriz1 + @matriz2).should == Matriz.new([[6,8],[10,12]])
     		end
+		it "Multiplicar matrices " do
+			(@matriz1 * @matriz2).should == Matriz.new([[19,22],[43,50]])
+        	end
 	end
 end
